aa6d21c1b6a8ebcc81627184dd514ef4685767e7
Eduardo <capamaral@gmail.com>
2023-12-02 14:09:30 -0300

    Create scan_hosts.sh

---
+++ scan_hosts.sh
@@ -0,0 +1,47 @@
+<!-- AUTO-GENERATED-CONTENT:START (STARTER) -->
+<p align="center">
+  <a href="https://www.faciltech.info">
+    <img alt="faciltech" src="https://yt3.ggpht.com/ytc/AAUvwng4KJs5t62nl2AvqDVOuXepKCSZp-l9_AQlsKBbwg=s176-c-k-c0x00ffffff-no-rj-mo" width="160" />
+  </a>
+</p>
+<h1 align="center">
+  Scan - Ferramenta para enumeraçao de portas.
+</h1>
+
+A ideia desse script simples otimizar tempo, utilizamos o Nmap para fazer a verificaçao das portas, em seguida trás as portas abertas e já enumera elas com alguns scripts do nmap.
+## 🚀 Por que usa-lo?
+
+Muitas vezes precisamos de um script para um determinado serviço, durante a busca das certificaçoes DCPT e OSCP, resolvi otimizar meu tempo, ao invés de dar comandos.. 
+  
+1.  **Como instalar?**
+
+    Navegue dentro de seu sistema, escolha o local e execute no terminal o comando abaixo.
+
+    ``` 
+        git clone https://github.com/faciltech/scan.git
+        Cloning into 'scan'...
+        remote: Enumerating objects: 10, done.
+        remote: Counting objects: 100% (10/10), done.
+        remote: Compressing objects: 100% (10/10), done.
+        remote: Total 10 (delta 1), reused 0 (delta 0), pack-reused 0
+        Receiving objects: 100% (10/10), 19.14 KiB | 612.00 KiB/s, done.
+        Resolving deltas: 100% (1/1), done.
+      ```
+
+2.  **Conceda permissão para o arquivo!**
+```
+chmod +x scan.sh
+```
+OBS: Uma outra boa dica é mover o arquivo do script para o diretório /usr/bin , dessa forma podemos utilizar de forma direta no terminal.
+## 🧐 Como usar o script?
+
+Você irá digitar ```./scan.sh``` 
+  
+<img alt="faciltech" src="20230118_194341.gif"/>
+## 🎓 Linguagem
+
+O utilitário é desenvolvido em linguagem shellscript, necessita que o nmap esteja instalado na maquina.
+
+<!-- AUTO-GENERATED-CONTENT:END -->
+
+
