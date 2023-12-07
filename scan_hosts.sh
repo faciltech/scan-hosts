f22f43c6a45035fc25a39f7977bb9ee190edd6ef
Eduardo <capamaral@gmail.com>
2023-12-07 07:13:45 -0300

    Update scan_hosts.sh
    Adicionado funçao de lista externa

--- scan_hosts.sh
+++ scan_hosts.sh
@@ -18,30 +18,52 @@ echo "DIGITE O NOME DO PROJETO: "
 read diretorio
 mkdir $diretorio
 #cd $diretorio
-hosts=$(sudo nmap -sn $1.0/24 | grep -oE '\b([0-9]{1,3}\.){3}[0-9]{1,3}\b')
-for i in $hosts;
-do
-        echo $i
-        mkdir $diretorio/$i
-done
-for i in $hosts;
-do
-      #  cd $i
-        echo ""
-        echo -e "\e[31m#### SCANEANDO PORTAS DO HOST $i ####\e[0m"
-        portas=$(sudo nmap -sS --open --source-port 443 -p- -Pn $i | grep '^[0-9]' | awk -F'/' '{print $1}'| xargs | sed 's/ /,/g')
+if [ $1 == "-l" ]
+then
+	hosts=$2
+	for i in $(cat $hosts);
+        do
+                echo $i
+                mkdir $diretorio/$i
+        done
+        for i in $(cat $hosts);
+        do
+                echo ""
+                echo -e "\e[31m#### SCANEANDO PORTAS DO HOST $i ####\e[0m"
+                portas=$(sudo nmap -sS --open --source-port 443 -p- -Pn $i | grep '^[0-9]' | awk -F'/' '{print $1}'| xargs | sed 's/ /,/g')
 
-        if [ -z "$portas" ]; then
-            echo "Sem portas abertas no Host $i"
-        else
-            printf "\e[1;33;40mPortas abertas do Host $i \e[0m: \e[1;32m$portas\e[0m"                                                                                                                                                      
-            echo " "                                                                                                                                                                                                                       
-            echo -e "\e[31m#### Informações dos serviços das Portas do Host $i ####\e[0m"                                                                                                                                                  
-            nmap -sC -sV -p $portas -Pn $i > $diretorio/$i/portas_servicos.txt                                                                                                                                                                           
-            echo "Os serviços das portas foram salvos no arquivo $diretorio/$i/portas_servicos.txt"
-#	    cd ..                                                                                                                                                  
-        fi                                                                                                                                                                                                                                 
-                                                                                                                                                                                                                                           
-       
-	                                                                                                                                                                                                                              
-done 
+                if [ -z "$portas" ]; then
+                	echo "Sem portas abertas no Host $i"
+                else
+                	printf "\e[1;33;40mPortas abertas do Host $i \e[0m: \e[1;32m$portas\e[0m"
+                	echo " "
+                	echo -e "\e[31m#### Informações dos serviços das Portas do Host $i ####\e[0m"
+                	nmap -sC -sV -p $portas -Pn $i > $diretorio/$i/portas_servicos.txt
+                	echo "Os serviços das portas foram salvos no arquivo $diretorio/$i/portas_servicos.txt"
+		fi
+        done 
+
+else
+	hosts=$(sudo nmap -sn $1.0/24 | grep -oE '\b([0-9]{1,3}\.){3}[0-9]{1,3}\b')
+	for i in $hosts;
+	do
+        	echo $i
+        	mkdir $diretorio/$i
+	done
+	for i in $hosts;
+	do
+        	echo ""
+        	echo -e "\e[31m#### SCANEANDO PORTAS DO HOST $i ####\e[0m"
+        	portas=$(sudo nmap -sS --open --source-port 443 -p- -Pn $i | grep '^[0-9]' | awk -F'/' '{print $1}'| xargs | sed 's/ /,/g')
+
+        	if [ -z "$portas" ]; then
+            	echo "Sem portas abertas no Host $i"
+        	else
+            	printf "\e[1;33;40mPortas abertas do Host $i \e[0m: \e[1;32m$portas\e[0m"
+            	echo " "
+            	echo -e "\e[31m#### Informações dos serviços das Portas do Host $i ####\e[0m"
+            	nmap -sC -sV -p $portas -Pn $i > $diretorio/$i/portas_servicos.txt
+            	echo "Os serviços das portas foram salvos no arquivo $diretorio/$i/portas_servicos.txt"
+        	fi
+	done
+fi 
